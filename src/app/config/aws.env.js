8a133d78c82a5884ce4df4bc3d1b4f7f36ff32d0
Darshan Raul <dar.raul@gmail.com>
2019-02-16 04:35:16 +0530

    Update aws.env.js

--- src/app/config/aws.env.js
+++ src/app/config/aws.env.js
@@ -1,8 +1,8 @@
 /// <reference types="aws-sdk" />
 const env = {
-	AWS_ACCESS_KEY: 'AKIAJ4DEMQ6FD2G6BGVQ', 
-	AWS_SECRET_ACCESS_KEY: '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', 
-	REGION : 'ap-south-1', 
+	AWS_ACCESS_KEY: 'xyz', 
+	AWS_SECRET_ACCESS_KEY: 'yxs', 
+	REGION : 'ap-', 
 	
 };
 

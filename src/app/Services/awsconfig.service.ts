6af2546c308f93853e56381e2c59c8c4b9ba6f4e
Darshan Raul <dar.raul@gmail.com>
2019-02-17 04:53:32 +0530

    Add files via upload

--- src/app/Services/awsconfig.service.ts
+++ src/app/Services/awsconfig.service.ts
@@ -1,137 +1,139 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { BehaviorSubject } from 'rxjs';
-
+import {
+	Injectable
+} from '@angular/core';
+import {
+	Observable,
+	of
+} from 'rxjs';
+import {
+	BehaviorSubject
+} from 'rxjs';
 
 
 const env = require('../config/aws.env.js');
 var AWS = require('aws-sdk');
-var IAM =require('aws-sdk/clients/iam');
+var IAM = require('aws-sdk/clients/iam');
 
-var S3 =require('aws-sdk/clients/s3');
- 
+var S3 = require('aws-sdk/clients/s3');
+
 //AWS.config.update({"accessKeyId": env.AWS_ACCESS_KEY, "secretAccessKey": env.AWS_SECRET_ACCESS_KEY, "region": env.REGION});
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 
 
 export class AwsconfigService {
-	
-AccessKey="";
-SecretAccessKey="";
-Region="";
 
-awsconfig: BehaviorSubject<boolean>;
+	AccessKey = "";
+	SecretAccessKey = "";
+	Region = "";
 
-  constructor() { 
-  this.awsconfig = new BehaviorSubject<boolean>(false);
-  }
+	awsconfig: BehaviorSubject < boolean > ;
 
- //config = false;
+	constructor() {
+		this.awsconfig = new BehaviorSubject < boolean > (false);
+	}
 
-
+	//config = false;
 
 
+	public getconfig(): Observable < boolean > {
+
+		return this.awsconfig.asObservable();
+
+	}
+
+	public setconfig(newValue: boolean): void {
+		this.awsconfig.next(newValue);
+		console.log(this.AccessKey + this.SecretAccessKey + this.Region);
+		AWS.config.update({
+			"accessKeyId": this.AccessKey,
+			"secretAccessKey": this.SecretAccessKey,
+			"region": this.Region
+		});
+		AWS.config.setPromisesDependency();
+	}
 
 
-public getconfig(): Observable<boolean> {
-	
-    return this.awsconfig.asObservable();
-	
-}
-
-public setconfig(newValue: boolean): void {
-    this.awsconfig.next(newValue);
-    console.log(this.AccessKey+this.SecretAccessKey+this.Region);
-    AWS.config.update({"accessKeyId": this.AccessKey, "secretAccessKey": this.SecretAccessKey, "region": this.Region});
-}
+	getiamusers() {
 
 
+		var iam = new IAM();
+
+		var params = {};
+		iam.listUsers(params, function (err, data) {
+			if (err) console.log(err, err.stack); // an error occurred
+			else console.log(data); // successful response
+
+		});
+	}
 
 
+	createiamuser() {
+		var iam = new IAM();
+		var params = {
+			UserName: "Bob"
+		};
+		iam.createUser(params, function (err, data) {
+			if (err) console.log(err, err.stack); // an error occurred
+			else console.log(data); // successful response
+			/*
+			data = {
+			 User: {
+			  Arn: "arn:aws:iam::123456789012:user/Bob", 
+			  CreateDate: <Date Representation>, 
+			  Path: "/", 
+			  UserId: "AKIAIOSFODNN7EXAMPLE", 
+			  UserName: "Bob"
+			 }
+			}
+			*/
+		});
+	}
 
-getiamusers(){
-	
-	
-	var iam = new IAM();
-	
- var params = {
- };
- iam.listUsers(params, function(err, data) {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     console.log(data);           // successful response
-   
- });
-}
+	create3bucket(bucketname) {
+		var s3 = new S3();
+		var params = {
+			Bucket: bucketname
+		};
 
+		s3.createBucket(params, function (err, data) {
+			if (err) console.log(err, err.stack); // an error occurred
+			else console.log(data); // successful response
 
-createiamuser(){
-	var iam = new IAM();
-var params = {
-  UserName: "Bob"
- };
- iam.createUser(params, function(err, data) {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     console.log(data);           // successful response
-   /*
-   data = {
-    User: {
-     Arn: "arn:aws:iam::123456789012:user/Bob", 
-     CreateDate: <Date Representation>, 
-     Path: "/", 
-     UserId: "AKIAIOSFODNN7EXAMPLE", 
-     UserName: "Bob"
-    }
-   }
-   */
- });
-}
-
-create3bucket(bucketname){
-var s3 = new S3();
-var params = {
-  Bucket: bucketname
- };
-
- s3.createBucket(params, function(err, data) {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     console.log(data);           // successful response
-  
- });
+		});
 
 		console.log(bucketname);
 
 
+	}
 
-}
 
+	createec2server(keypair, count) {//////////////////WORKING////////////////////////////
+		
+		var EC2 = require('aws-sdk/clients/ec2');
+		var ec2 = new AWS.EC2();
 
-createec2server(keypair,count){
-	var EC2 =require('aws-sdk/clients/ec2');
-var ec2 = new AWS.EC2();
+		var params = {
 
-var params = {
-  
-  ImageId: "ami-0d773a3b7bb2bb1c1", 
-  InstanceType: "t2.micro", 
-  KeyName: keypair , 
-  MaxCount: count, 
-  MinCount: count, 
-  
- };
- ec2.runInstances(params, function(err, data) {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     console.log(data);           // successful response
-   /*
-   data = {
-   }
-   */
- });
+			ImageId: "ami-0d773a3b7bb2bb1c1",
+			InstanceType: "t2.micro",
+			KeyName: keypair,
+			MaxCount: count,
+			MinCount: count,
 
-}
+		};
+		ec2.runInstances(params, function (err, data) {
+			if (err) console.log(err, err.stack); // an error occurred
+			else console.log(data); // successful response
+			/*
+			data = {
+			}
+			*/
+		});
 
+	}
 
 
 }

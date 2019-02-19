7c10cd8e3128f3ce2e5fa6221dd6b6f8b0db1834
Darshan Raul <dar.raul@gmail.com>
2019-02-19 15:25:22 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,48 @@
+import flask
+from flask import request, jsonify
+import boto3
+s3=boto3.client('s3')
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>aws api</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+@app.route('/buckets/all', methods=['GET'])
+def bucket_all():
+    buckets=s3.list_buckets()
+    bucketlist=[]
+    for i in buckets['Buckets']:
+        bucket= i['Name']
+        bucketlist.append(bucket)
+    
+    return  "darshan's bucket list" + str(bucketlist)
+	
+
+
+@app.route('/buckets', methods=['GET'])
+def bucektcontent():
+    
+    if 'bucketname' in request.args:
+        bucketname = str(request.args['bucketname'])
+    else:
+        return "Error: No bucketname field provided. Please specify an bucketname."
+
+    
+    results = []
+
+    
+    objects=s3.list_objects(Bucket=str(bucketname))
+
+   
+    return jsonify(objects[Contents])
+
+app.run(port=7860)
